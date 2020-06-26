ceefa8b7b5354d251f2c1d57b75f57a296ab8ba1
askirmas <kirmas@phystech.edu>
2020-06-26 22:38:59 +0300

    Add falsy type alias

--- types/natives.d.ts
+++ types/natives.d.ts
@@ -5,6 +5,7 @@ interface ObjectConstructor {
   keys<T extends ArrayLike<any>>(o: T): string[]
   keys<T extends {}>(o: T): (string & keyof T)[]
   entries<T extends {[k: string]: any}>(o: T): [string & keyof T, T[keyof T]][]
+  /** Only from es2019 */
   fromEntries<T extends ArrayLike<[string, any]>>(o: T): {[key in T[number][0]]: T[number][1]}
 }
 
--- types/ontology.d.ts
+++ types/ontology.d.ts
@@ -5,3 +5,6 @@ type JsonStructure<T = any> = T[] | {[key: string]: T}
 // Changing here to Structured makes Jsoned Circular
 type JsonNative = JsonPrimitive | JsonNative[] | {[property: string]: JsonNative} 
 type JsonReviver<T> = (this: JsonStructure<T>, key: string, value: JsonStructure<T>) => T
+
+/** NB! Without `NaN` https://developer.mozilla.org/en-US/docs/Glossary/Falsy */
+type falsy = false | 0 | -0 | 0n | "" | null | undefined // | NaN
