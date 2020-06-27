d9e6bfea9bd686fccd399c50e79073c7431f93f3
askirmas <kirmas@phystech.edu>
2020-06-27 04:49:26 +0300

    Remove from Object.fromEntries notation

--- types/es.object.d.ts
+++ types/es.object.d.ts
@@ -5,5 +5,5 @@ interface ObjectConstructor {
   keys<T extends {}>(o: T): (string & keyof T)[]
   entries<T extends {[k: string]: any}>(o: T): [string & keyof T, T[keyof T]][]
   /** Only from es2019 */
-  fromEntries<T extends ArrayLike<[string, any]>>(o: T): {[key in T[number][0]]: T[number][1]}
+  // TODO fromEntries
 }
