b212369bf6ef3da5a245cf59edb353d92ab685a8
askirmas <kirmas@phystech.edu>
2020-06-27 04:48:11 +0300

    Move collection-has issue comment to test

--- sandbox/set.test.ts
+++ sandbox/set.test.ts
@@ -1,3 +1,5 @@
+/** Issue https://www.typescriptlang.org/play/#code/MYewdgzgLgBAtgTwMoFNYF4ZhQdxqqAHmgCcBLMAcwD4AKASgCgZGAzAVzGCjPBgAsAhhFoAPAFwxSFSjAA+WdnABGKEvRgBvZjF0k07EmHjI0AOiEjRTAL5A */
+
 const mySet = new Set<string|undefined>()
 , mySet_Input = 1
 , mySet_O1: false = mySet.has(mySet_Input)
--- types/es.collection.d.ts
+++ types/es.collection.d.ts
@@ -1,14 +1,13 @@
 /// <reference lib="es5" />
 
-
-/** Issue https://www.typescriptlang.org/play/#code/MYewdgzgLgBAtgTwMoFNYF4ZhQdxqqAHmgCcBLMAcwD4AKASgCgZGAzAVzGCjPBgAsAhhFoAPAFwxSFSjAA+WdnABGKEvRgBvZjF0k07EmHjI0AOiEjRTAL5A */
 interface Set<T> {
   has<V>(value: V): (V & T) extends never ? false : boolean 
+  /** Fallback */
   has(value: any): boolean
 }
 
-/** Issue https://www.typescriptlang.org/play/#code/MYewdgzgLgBAtgTwMoFNYF4ZhQdxqqAHmgCcBLMAcwD4AKASgCgZGAzAVzGCjPBgAsAhhFoAPAFwxSFSjAA+WdnABGKEvRgBvZjF0k07EmHjI0AOiEjRTAL5A */
 interface Map<K, V> {
   has<T>(value: T): (K & T) extends never ? false : boolean 
+  /** Fallback */
   has(key: any): boolean 
 }
