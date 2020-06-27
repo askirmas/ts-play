c2bd99a11d8f81d1d770bd29ef0256461424dfa0
askirmas <kirmas@phystech.edu>
2020-06-27 13:22:10 +0300

    Add same notation to weak collections

--- types/es.collection.d.ts
+++ types/es.collection.d.ts
@@ -6,8 +6,18 @@ interface Set<T> {
   has<V>(value: V): onIntersection<T, V, boolean, false>
   delete<V>(value: onIntersection<T, V, V, never>): boolean
 }
+interface WeakSet<T> {
+  has<V>(value: V): onIntersection<T, V, boolean, false>
+  delete<V>(value: onIntersection<T, V, V, never>): boolean
+}
 
 interface Map<K, V> {
   has<T>(value: T): onIntersection<K, T, boolean, false>
   delete<T>(value: onIntersection<K, T, T, never>): boolean
+  // TODO: Add get
+}
+interface WeakMap<K, V> {
+  has<T>(value: T): onIntersection<K, T, boolean, false>
+  delete<T>(value: onIntersection<K, T, T, never>): boolean
+  // TODO: Add get
 }
