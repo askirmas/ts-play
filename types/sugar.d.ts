cb9f1d17ad375a536c744204a9e7c2f1f63bf600
askirmas <kirmas@phystech.edu>
2020-06-29 19:31:09 +0300

    Add tuple check generics

--- types/sugar.d.ts
+++ types/sugar.d.ts
@@ -3,8 +3,10 @@
 */
 declare type Dict<V = unknown> = Record<string, V>
 
-/** Empty object */
-declare type Empty = Record<never, unknown>
+/** Empty object */ //TODO <never, never> ?
+declare type EmptyObject = Record<never, unknown>
+/** Empty array */
+declare type EmptyArray = never[]
 
 /** Just `function` */
 declare type Fn<A extends unknown[], R, T = unknown> = (this: T, ...parameters: A) => R
@@ -15,3 +17,18 @@ declare type Maybe<T> = undefined | T
 
 /** Include `null` and `undefined` to T */
 declare type Nullable<T> = null | undefined | T
+
+declare type AreEqualE<T1, T2> = Exclude<T1, T2> extends never ? Exclude<T2, T1> extends never ? true : false : false
+declare type AreEqualD<T1, T2> = T1 extends T1&T2 ? T2 extends T1&T2 ? true : false : false
+declare type AreEqualC<T1, T2> = T1|T2 extends T1 ? T1|T2 extends T2 ? true : false : false
+
+//declare type Keyof<T> = T extends Record<infer K, unknown> ? K : never
+
+// Magic
+declare type IsTupleL<T extends readonly unknown[]> = number extends T["length"] ? false : true
+declare type IsTupleI<T extends readonly unknown[]> = string extends Indexes<T> ? false : true
+
+
+// TODO Proto = Array
+declare type Indexes<T extends readonly unknown[], Proto = EmptyArray> = Exclude<keyof T, keyof Proto>
+declare type OwnTypes<T extends readonly unknown[], Proto = Object> = Exclude<keyof T, keyof Proto>
