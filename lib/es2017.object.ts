b43744e50ac260937b115ea017b727123a6499dd
askirmas <kirmas@phystech.edu>
2020-08-01 01:47:32 +0300

    Add accurate Object.entries typing

--- lib/es2017.object.ts
+++ lib/es2017.object.ts
@@ -1,5 +1,5 @@
 /// <reference lib="es2017.object" />
 
 interface ObjectConstructor {
-  entries<T extends {[k: string]: any}>(o: T): [string & keyof T, T[keyof T]][]
+  entries<T extends {[k: string]: any}>(o: T): {[P in keyof T]: [P, T[P]]}[keyof T][]
 }
